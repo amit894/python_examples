d2af7e545bb89c8287184808d29519bb0a1e7666
amit894 <amit.894@gmail.com>
2021-04-07 17:36:37 +0530

    Adding python examples

--- practice/src/regex/regex.py
+++ practice/src/regex/regex.py
@@ -1,1 +1,49 @@
 import sys
+import
+
+
+# ^ Begins with in Regex
+# $ Ends with in Regex
+# * Zero or more occurences
+# + One or more occurences
+# {} exact number of occurences
+
+for lines in sys.stdin: #Seaching for character in Stdin
+    print(lines)
+    m = re.search('x',lines)
+    try:
+        m.group(0)
+    except (TypeError,AttributeError):
+        print("Match Not found")
+
+for lines in sys.stdin:
+    print(lines)
+    m = re.split('x',lines) #Splitting by character in Stdin
+    try:
+        print(m)
+    except (TypeError,AttributeError):
+        print("Match Not found")
+
+for lines in sys.stdin: #Seaching for starts with in Stdin
+    print(lines)
+    m = re.search('^Amit',lines)
+    try:
+        m.group(0)
+    except (TypeError,AttributeError):
+        print("Match Not found")
+
+for lines in sys.stdin: #Splits for any occurence of a lower alphabet
+    print(lines)
+    m = re.split('[a-z]',lines)
+    try:
+        print(m)
+    except (TypeError,AttributeError):
+        print("Match Not found")
+
+for lines in sys.stdin: #Splits for any occurence of a set of alphabets
+    print(lines)
+    m = re.split('[a-z]*',lines)
+    try:
+        print(m)
+    except (TypeError,AttributeError):
+        print("Match Not found")
