122a5be58b08e216ede9c5e70fb743cf9af2082a
amit894 <amit.894@gmail.com>
2021-04-11 20:21:58 +0530

    Answers to Hacker Rank Basic Problem Solving

---
+++ practice/src/lists/road_repair.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'getMinCost' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY crew_id
+#  2. INTEGER_ARRAY job_id
+#
+
+def getMinCost(crew_id, job_id):
+    # Write your code here
+    crew_id.sort()
+    job_id.sort()
+    distance=0
+    for i in range(len(crew_id)):
+        if(job_id[i]>=crew_id[i]):
+            distance=distance+(job_id[i]-crew_id[i])
+        else:
+            distance=distance+(crew_id[i]-job_id[i])
+    return(distance)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    crew_id_count = int(input().strip())
+
+    crew_id = []
+
+    for _ in range(crew_id_count):
+        crew_id_item = int(input().strip())
+        crew_id.append(crew_id_item)
+
+    job_id_count = int(input().strip())
+
+    job_id = []
+
+    for _ in range(job_id_count):
+        job_id_item = int(input().strip())
+        job_id.append(job_id_item)
+
+    result = getMinCost(crew_id, job_id)
+    print(result)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
