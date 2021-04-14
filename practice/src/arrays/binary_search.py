6a2068c9cf1d71f13132e501d744fe417bcbf66a
amit894 <amit.894@gmail.com>
2021-04-14 19:06:07 +0530

    Fixing Binary search algo

--- practice/src/arrays/binary_search.py
+++ practice/src/arrays/binary_search.py
@@ -1,19 +1,19 @@
 def binary_search(A,low,high,item):
 
     if(high>=low):
-        mid = (high+low)//2
-        if (item>=A[mid]):
-            binary_search(A,mid+1,high,item) # Item bigger than the middle condition loop
+        mid=(high+low)//2
+        if (item==A[mid]):
+            return mid
+        if (item>A[mid]):
+            return binary_search(A,mid+1,high,item) # Item bigger than the middle condition loop
         else:
-            binary_search(A,low,mid-1,item) # Item smaller than the middle condition loop
+            return binary_search(A,low,mid-1,item) # Item smaller than the middle condition loop
     else:
         return (-1) # Recurrsion base condition
-    return mid
 
-
-A = [1,2,3,4,5]
+A = [1,2,3,4]
 n =len(A)
-x=binary_search(A,0,n-1,2)
+x=binary_search(A,0,n-1,4)
 print(x)
 
 #Time Complexity : O(log n)
