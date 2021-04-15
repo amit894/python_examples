b97c723a0e698a4f2d6f0d080d0dd6ebcd1e8624
amit894 <amit.894@gmail.com>
2021-04-15 15:06:45 +0530

    Stacked Linked List Optimizations

--- practice/src/stacks/stack_linked_list.py
+++ practice/src/stacks/stack_linked_list.py
@@ -1,65 +1,93 @@
 class Node:
-	def __init__(self, data):
-		self.data=data
-		self.next=None
-
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
 class Stack:
+    def __init__(self):
+        self.head=None
 
-	def __init__(self):
-		self.head = None
 
-	def isempty(self):
-		if(self.head==None):
-			return True
+    def stack_size(self):
+        current_node=self.head
+        count=0
+        if(current_node==None):
+            return 0
+        else:
+            while(current_node!=None):
+                count+=1
+                current_node=current_node.next
+        return count
 
-	def size_stack(self):
-		if self.isempty():
-			return 0
-		else:
-			element=self.head
-			size = 0
-			while(element!=None):
-				size=size+1
-				element=element.next
-			return size
+    def print_stack(self):
+        current_node=self.head
+        if(current_node==None):
+            print("Empty Stack")
+            return
+        else:
+            while(current_node!=None):
+                print("Data Element %d"%current_node.data)
+                current_node=current_node.next
 
-	def print_stack(self):
-		if self.isempty():
-			return 0
-		else:
-			element=self.head
-			while(element!=None):
-				print("%d" %element.data)
-				element=element.next
+    def peek(self):
+        current_node=self.head
+        if(current_node==None):
+            print("Empty Stack")
+            return
+        else:
+            while(current_node.next!=None):
+                current_node=current_node.next
+        print("Data Element %d" %current_node.data)
 
-	def peek(self):
-		if self.isempty():
-			print ("Empty stack nothing to peek")
-		else:
-			element=self.head
-			while(element.next!=None):
-				element=element.next
-			print ("Last element of the stack is %d" %element.data)
 
-	def push(self,node):
-		if self.isempty():
-			self.head=node
-		else:
-			element=self.head
-			while(element.next!=None):
-				element=element.next
-			element.next=node
+    def pop(self):
+        current_node=self.head
+        count=0
+        if(current_node==None):
+            print("Empty Stack")
+            return
+        else:
+            while(current_node.next!=None):
+                temp=current_node
+                current_node=current_node.next
+            temp.next=None
 
-	def pop(self):
-		if self.isempty():
-			print ("Empty stack nothing to pop")
-		else:
-			element=self.head
-			while(element.next!=None):
-				 prev=element
-				 element=element.next
-			prev.next=None
+
+    def push(self,node):
+        current_node=self.head
+        if(current_node==None):
+            self.head=node
+            return
+        else:
+            while(current_node!=None):
+                temp=current_node
+                current_node=current_node.next
+            temp.next=node
+
+
+S1=Stack()
+print("De limiter")
+S1.peek()
+S1.pop()
+S1.push(Node(2))
+print("De limiter")
+S1.peek()
+S1.push(Node(4))
+print("De limiter")
+S1.peek()
+S1.push(Node(6))
+print("De limiter")
+S1.peek()
+S1.push(Node(8))
+print("De limiter")
+S1.peek()
+S1.push(Node(10))
+print("De limiter")
+print("De limiter")
+S1.peek()
+print("De limiter")
+S1.print_stack()
+
 
 S1=Stack()
 S1.peek()
