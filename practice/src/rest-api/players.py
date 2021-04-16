38f9ce500e095007457bb289ea3fe6c5cccdaa04
Amit Raj <amit.raj@walmart.com>
2021-04-16 09:55:15 +0530

    Adding post functionality

--- practice/src/rest-api/players.py
+++ practice/src/rest-api/players.py
@@ -1,38 +1,50 @@
 import flask
 from flask import request,jsonify
+import json
+
 
 players = [
-{
-'id':0,
-'player_name':'Sachin'
-},
-{
-'id':1,
-'player_name':'Ricky'
-},
-{
-'id':2,
-'player_name':'Virat'
-},
-{
-'id':3,
-'player_name':'Kevin'
-}
 ]
 
 app=flask.Flask(__name__)
 app.config["DEBUG"]=True
 
+def readPlayers():
+    try:
+        f1=open('../../resources/players.json','r')
+        players=json.load(f1)
+        f1.close()
+        return players
+    except:
+        return ("Error reading from file")
+        f1.close()
 
+def addPlayers(data):
+    players=[]
+    try:
+        players=readPlayers()
+        players.append(data)
+    except:
+        print("Error reading from file")
+
+    try:
+        f2=open('../../resources/players.json','w')
+        json.dump(players,f2)
+        f2.close()
+        return ("Player added")
+    except:
+        f2.close()
+        return ("Player couldnt be added")
 
 @app.route('/all', methods=['GET'])
 def getAllPlayer():
-    return jsonify(players)
+    return jsonify(readPlayers())
 
 
 @app.route('/player',methods=['GET'])
-def getBook():
+def getPlayer():
     player_id=0
+    players=readPlayers()
     if 'id' in request.args:
         player_id=int(request.args['id'])
     else:
@@ -44,6 +56,11 @@ def getBook():
         print(player,player_id)
         if player['id']==player_id:
             return player
+
+@app.route('/addplayer',methods=['POST'])
+def addPlayer():
+    data=request.json
+    return jsonify(addPlayers(data))
 
 @app.errorhandler(404)
 def page_not_found(e):
