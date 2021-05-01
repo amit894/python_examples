b7a50b3b1c4c997f5c016bb02fa86a805c239d45
Amit Raj <amit.raj@walmart.com>
2021-05-01 21:25:35 +0530

    Fixing stack and queuue

--- practice/src/queue/queue_linked_list.py
+++ practice/src/queue/queue_linked_list.py
@@ -6,58 +6,58 @@ class Node:
 class Queue:
     def __init__(self):
         self.head=None
+        self.rear=None
+
+    def is_stack_empty(self):
+        if (self.head==None):
+            return True
+        else:
+            return False
 
     def queue_size(self):
         count=0
-        current_node=self.head
-        if(current_node==None):
+        if(self.is_stack_empty()):
             return 0
         else:
+            current_node=self.head
             while(current_node!=None):
                 count+=1
                 current_node=current_node.next
         return count
 
     def print_queue(self):
-        current_node=self.head
-        if(current_node==None):
+        if(self.is_stack_empty()):
             print ("No elements in the queue")
         else:
+            current_node=self.head
             while(current_node!=None):
                 print ("Data is %d" %current_node.data)
                 current_node=current_node.next
 
-    def peek(self):
-        if(self.head==None):
-            print ("No elements in the queue nothing to peek")
+
+    def EnQueue(self,node):
+        if(self.is_stack_empty()):
+            self.head=self.rear=node
         else:
-            print ("Data is %d" %self.head.data)
+            self.rear.next=node
+            self.rear=node
 
-
-    def pop(self):
-        if(self.head==None):
-            print ("No elements in the queue nothing to pop")
+    def DeQueue(self):
+        if(self.is_stack_empty()):
+            print ("No elements in the queue")
         else:
             self.head=self.head.next
-
-    def push(self,node):
-        if(self.head!=None):
-            node.next=self.head
-        self.head=node
+            if(self.head==None):
+                self.rear=None
 
 Q1=Queue()
-Q1.peek()
-Q1.pop()
-Q1.push(Node(2))
-Q1.push(Node(4))
-Q1.push(Node(6))
-Q1.push(Node(8))
-Q1.peek()
-Q1.pop()
-Q1.pop()
-Q1.peek()
-Q1.push(Node(4))
-Q1.push(Node(6))
-Q1.push(Node(8))
+Q1.DeQueue()
+Q1.EnQueue(Node(2))
+Q1.EnQueue(Node(4))
+Q1.EnQueue(Node(6))
+Q1.EnQueue(Node(8))
+print("De limiter")
+Q1.print_queue()
+Q1.DeQueue()
 print("De limiter")
 Q1.print_queue()
