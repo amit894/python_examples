eb263f2095645da10c8f656b4ba344a66c1d9277
amit894 <amit.894@gmail.com>
2021-05-04 21:59:41 +0530

    Fix Sorting algos

--- practice/src/sorting/merge_sort.py
+++ practice/src/sorting/merge_sort.py
@@ -1,30 +1,30 @@
-def mergeSort(arr):
-    if len(arr)>1:
-        mid=len(arr)//2
-        L=arr[:mid]
-        R=arr[mid:]
+def mergeSort(A):
+    if len(A)>1:
+        mid=len(A)//2
+        L=A[:mid]
+        R=A[mid:]
 
         mergeSort(L)
         mergeSort(R)
 
         i=j=k=0
 
-        while i<len(L) and j<len(L):
+        while i<len(L) and j<len(R) and k<len(A):
             if L[i]<R[j]:
-                arr[k]=L[i]
+                A[k]=L[i]
                 i+=1
             else:
-                arr[k]=R[j]
+                A[k]=R[j]
                 j+=1
             k+=1
 
         while i<len(L):
-            arr[k]=L[i]
+            A[k]=L[i]
             i+=1
             k+=1
 
         while j<len(R):
-            arr[k]=R[j]
+            A[k]=R[j]
             j+=1
             k+=1
 
@@ -33,10 +33,11 @@ mergeSort(A)
 print(A)
 
 
-#Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+#Time Complexity: Sorting Aays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
 #T(n) = 2T(n/2) + θ(n)
+#O(n*log n)
 
-#The above recurrence can be solved either using the Recurrence Tree method or the Master method. It falls in case II of Master Method and the solution of the recurrence is θ(nLogn). Time complexity of Merge Sort is  θ(nLogn) in all 3 cases (worst, average and best) as merge sort always divides the array into two halves and takes linear time to merge two halves.
+#The above recurrence can be solved either using the Recurrence Tree method or the Master method. It falls in case II of Master Method and the solution of the recurrence is θ(nLogn). Time complexity of Merge Sort is  θ(nLogn) in all 3 cases (worst, average and best) as merge sort always divides the Aay into two halves and takes linear time to merge two halves.
 #Auxiliary Space: O(n)
 #Algorithmic Paradigm: Divide and Conquer
 #Sorting In Place: No in a typical implementation
--- practice/src/sorting/quick_sort.py
+++ practice/src/sorting/quick_sort.py
@@ -19,3 +19,24 @@ def quick_sort(arr,low,high):
 A = [56, 21, 21, 32, 43]
 quick_sort(A,0,len(A)-1)
 print(A)
+
+
+# Worst Case: The worst case occurs when the partition process always picks greatest or smallest element as pivot. If we consider above partition strategy where last element is always picked as pivot, the worst case would occur when the array is already sorted in increasing or decreasing order. Following is recurrence for worst case.
+#
+#  T(n) = T(0) + T(n-1) + \theta(n)
+# which is equivalent to
+# T(n) = T(n-1) + \theta(n)
+# The solution of above recurrence is  \theta(n2).
+
+
+# Best Case: The best case occurs when the partition process always picks the middle element as pivot. Following is recurrence for best case. 
+#
+#  T(n) = 2T(n/2) + \theta(n)
+# The solution of above recurrence is \theta        (nLogn). It can be solved using case 2 of Master Theorem.
+#
+# Average Case:
+# To do average case analysis, we need to consider all possible permutation of array and calculate time taken by every permutation which doesn’t look easy.
+# We can get an idea of average case by considering the case when partition puts O(n/9) elements in one set and O(9n/10) elements in other set. Following is recurrence for this case.
+#
+#  T(n) = T(n/9) + T(9n/10) + \theta(n)
+# Solution of above recurrence is also O(nLogn)
