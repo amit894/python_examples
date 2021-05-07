f2df0c8f1f9570fcf16ae6a7fbcf84c5ec634c30
amit894 <amit.894@gmail.com>
2021-05-07 08:44:55 +0530

    Some more Algo's

--- practice/src/arrays/new_adj_swaps.py
+++ practice/src/arrays/new_adj_swaps.py
@@ -1,13 +1,12 @@
 def minswaps(arr):
     count=0
-    unplaced_zeros=0
+    unplaced_zeroes=0
     for i in range(len(arr)-1,-1,-1):
-        if(arr[i]==0):
-            unplaced_zeros+=1
+        if arr[i]==0:
+            unplaced_zeroes+=1
         else:
-            count+=unplaced_zeros
+            count+=unplaced_zeroes
     return count
-
 
 arr = [0, 0, 1, 0, 1, 0, 1, 1]
 print(minswaps(arr))
