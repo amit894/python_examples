20eb07603154e2721ab25dce31e63c77c1243799
amit894 <amit.894@gmail.com>
2021-05-12 16:31:35 +0530

    Delete Nth node from end

---
+++ practice/src/facebook_leetcode/delete_from_end.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+def removeNthFromEnd(head,n):
+    totalNodes=countNodes(head)
+    print(totalNodes)
+    deleteNode(head,totalNodes-n+1)
+    return head
+
+def countNodes(head):
+    if head==None:
+        return 0
+    else:
+        count=0
+        current_node=head
+        while(current_node):
+            count+=1
+            current_node=current_node.next
+        return count
+
+def deleteNode(head,m):
+    if head==None:
+        return None
+    else:
+        count=0
+        prev_node=None
+        current_node=head
+        while(current_node):
+            if(count==m-1):
+                break
+            count+=1
+            prev_node=current_node
+            current_node=current_node.next
+        if prev_node==None:
+            head=current_node
+        else:
+            prev_node.next=current_node.next
+
+head=Node(1)
+removeNthFromEnd(head,1)
