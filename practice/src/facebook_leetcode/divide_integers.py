13ce01f44320556646fcd77a8524ef3e529dab1c
amit894 <amit.894@gmail.com>
2021-05-16 22:35:56 +0530

    Boundary Conditions

--- practice/src/facebook_leetcode/divide_integers.py
+++ practice/src/facebook_leetcode/divide_integers.py
@@ -4,11 +4,20 @@ def divideNumbers(dividend,divisor):
     signed_divisor=abs(divisor)
     max_overflow_value= pow(2,31)-1
     min_overflow_value= -1*pow(2,31)
-    while(signed_dividend>=signed_divisor):
-        signed_dividend-=signed_divisor
-        quotient+=1
-    if dividend*divisor<=0:
-        quotient=-1*quotient
-    return quotient
+    if (signed_divisor==1):
+        quotient=divisor*dividend
+        if quotient>=max_overflow_value:
+            return(max_overflow_value)
+        elif quotient<=min_overflow_value:
+            return(min_overflow_value)
+        else:
+            return quotient
+    else:
+        while(signed_dividend>=signed_divisor):
+            signed_dividend-=signed_divisor
+            quotient+=1
+        if dividend*divisor<=0:
+            quotient=-1*quotient
+        return quotient
 
-print(divideNumbers(2147483647,2147483647))
+print(divideNumbers(2147483647,2))
