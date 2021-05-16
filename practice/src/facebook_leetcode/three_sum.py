f6550123a300f65a70cc3137ff2c19ff9b2e110d
amit894 <amit.894@gmail.com>
2021-05-16 15:53:32 +0530

    Adding Three Sum fix

--- practice/src/facebook_leetcode/three_sum.py
+++ practice/src/facebook_leetcode/three_sum.py
@@ -1,19 +1,20 @@
 def three_sum(A):
     A.sort()
-    print(A)
     indexes=[]
-    for i in range(len(A)-2):
+    for i in range(len(A)):
         l=i+1
         r=len(A)-1
-        while(l<=r):
-            if (A[i]+A[l]+A[r]==0) and (l!=r and i!=l and r!=i):
-                indexes.append([A[i],A[l],A[r]])
-                break
-            elif A[i]+A[l]+A[r]<0:
+        while(l<r):
+            if (A[i]+A[l]+A[r]==0):
+                if([A[i],A[l],A[r]] not in indexes):
+                    indexes.append([A[i],A[l],A[r]])
                 l+=1
-            else:
                 r-=1
+            elif (A[i]+A[l]+A[r]<0):
+                l+=1
+            elif (A[i]+A[l]+A[r]>0):
+                r=r-1
     return indexes
 
-A=[-1,0,1,2,-1,-4]
+A=[-2,0,1,1,2]
 print(three_sum(A))
