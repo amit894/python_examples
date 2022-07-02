148b85a5ff75f483f3f6e8b9d7153b86144e29e6
Amit Raj <amit.894@gmail.com>
2022-07-02 22:34:58 +0530

    Adding mongodb samples

---
+++ practice/src/mongodb/find.py
@@ -0,0 +1,10 @@
+
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
+
+# set our database to "cooker"
+db = client.cooker
+
+for item in db.recipes.find():
+    print(item["title"])
